52ef5249541798d7b399e967037aefd2f1345204
SpMex <113365369+yungmexx@users.noreply.github.com>
2023-06-29 09:41:09 -0700

    Update Client.lua

--- Client/Client.lua
+++ Client/Client.lua
@@ -1,16 +1,27 @@
--- Example usage:
-    -- exports["v-requirements"]:ShowRequirements({
-    --     {
-    --         ["Amount"] = 1,
-    --         ["Image"] = "qs-inventory/web/images/gold_ring.png",
-    --         ["Label"] = rl.Shared.Items["gold_ring"].label
-    --     },
-    --     {
-    --         ["Amount"] = 21,
-    --         ["Image"] = "qs-inventory/web/images/sapphire_earrings.png",
-    --         ["Label"] = rl.Shared.Items["sapphire_earrings"].label
-    --     },
-    -- })
+--[[
+Example for qs-inventory:
+
+         {
+             ["Amount"] = 21,
+             ["Image"] = "qs-inventory/html/images/sapphire_earrings.png",
+             ["Label"] = QBCore.Shared.Items["sapphire_earrings"].label
+         },
+Example for ox_inventory:
+
+         {
+             ["Amount"] = 1,
+             ["Image"] = "ox_inventory/web/images/gold_ring.png",
+             ["Label"] = QBCore.Shared.Items["gold_ring"].label
+         },
+
+Example for qb-inventory:
+
+         {
+             ["Amount"] = 1,
+             ["Image"] = "qb-inventory/html/images/gold_ring.png",
+             ["Label"] = QBCore.Shared.Items["gold_ring"].label
+         },
+--]]
 
 -- Example remove:
     -- exports["v-requirements"]:HideRequirements()
